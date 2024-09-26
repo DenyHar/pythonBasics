e45b5cac5ca195aecc85fb1fa4a52735e9cdfb66
DenyHar <2denshik2@gmail.com>
2024-09-26 10:55:39 +0300

    first

---
+++ lab_2_Harahulia/lab_2_Harahulia.py
@@ -0,0 +1,20 @@
+print("Programming", "Essential", "in", sep="***", end="...")
+print("Python")
+
+print(4 * " " + "*")
+print(3 * " " + "*", "*", sep=" ")
+print(2 * " " + "*", "*", sep=3 * " ")
+print(" *", "*", sep=5 * " ")
+print(3 * "*", 3 * "*", sep=3 * " ")
+print((2 * " " + "*" + 3 * " " + "*\n") * 2, end="")
+print(2 * " " + 5 * "*")
+
+print("I\'m student")
+
+print("\"I\'m\"\n", "\"\"learning\"\"\n", "\"\"\"Python\"\"\"", sep="")
+
+print("500\u2088 в десятковій дорівнює ", 0o500)
+
+print("777\u2081\u2086 в десятковій дорівнює ", 0x777)
+
+print(3**3**4)
